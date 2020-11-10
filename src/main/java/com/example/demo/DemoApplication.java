12010509302b189ac45a90c773aafe1a1f04ba86
Alexander Ang <alexanderang.24@gmail.com>
2020-11-10 22:37:55 +0800

    Java Strings Introduction

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,27 +2,27 @@ package com.example.demo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.text.NumberFormat;
-import java.util.Locale;
 import java.util.Scanner;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double payment = scanner.nextDouble();
-        scanner.close();
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        System.out.println(A.length() + B.length());
 
-        // Write your code here.
-        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
-        String india = NumberFormat.getCurrencyInstance(new Locale("en", "in")).format(payment);
-        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
-        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+        if(A.compareTo(B) <= 0) {
+            System.out.println("No");
+        } else {
+            System.out.println("Yes");
+        }
 
-        System.out.println("US: " + us);
-        System.out.println("India: " + india);
-        System.out.println("China: " + china);
-        System.out.println("France: " + france);
+        A = A.substring(0, 1).toUpperCase() + A.substring(1);
+        B = B.substring(0, 1).toUpperCase() + B.substring(1);
+
+        System.out.println(A + " " + B);
     }
 }
