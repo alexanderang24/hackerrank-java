cad3c59ee4ad676eaf737a0075b0b6fbe5d09a92
Alexander Ang <alexanderang.24@gmail.com>
2020-11-10 22:50:22 +0800

    Java Substring Comparisons

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,10 +9,34 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String S = in.next();
-        int start = in.nextInt();
-        int end = in.nextInt();
+        String s = in.next();
+        int k = in.nextInt();
 
-        System.out.println(S.substring(start, end));
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        String temp = "";
+        for (int i = 0; i <= s.length() - k; i++) {
+            temp = s.substring(i, i + k);
+
+            if(smallest.equals("")) smallest = temp;
+            if(largest.equals("")) largest = temp;
+
+            if (temp.compareTo(smallest) < 0) {
+                smallest = temp;
+            }
+
+            if (temp.compareTo(largest) > 0) {
+                largest = temp;
+            }
+        }
+        return smallest + "\n" + largest;
     }
 }
