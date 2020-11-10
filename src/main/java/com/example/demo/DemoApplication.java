fe8504ce2d58f0d2a61640cf328696dd6257d555
Alexander Ang <alexanderang.24@gmail.com>
2020-11-10 22:39:03 +0800

    Java Substring

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,21 +8,11 @@ import java.util.Scanner;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        String B=sc.next();
-        /* Enter your code here. Print output to STDOUT. */
-        System.out.println(A.length() + B.length());
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
 
-        if(A.compareTo(B) <= 0) {
-            System.out.println("No");
-        } else {
-            System.out.println("Yes");
-        }
-
-        A = A.substring(0, 1).toUpperCase() + A.substring(1);
-        B = B.substring(0, 1).toUpperCase() + B.substring(1);
-
-        System.out.println(A + " " + B);
+        System.out.println(S.substring(start, end));
     }
 }
