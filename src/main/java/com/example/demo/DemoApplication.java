5cde336761e43af5b3eca91ab80b1dd9e98dd132
Alexander Ang <alexanderang.24@gmail.com>
2020-11-10 21:22:41 +0800

    Java String Reverse

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,36 +10,22 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String a = sc.next();
-        String b = sc.next();
-        System.out.println(isAnagram(a, b));
-    }
+        String s =sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        boolean flag = true;
+        int loop = s.length() / 2;
 
-    static boolean isAnagram(String a, String b) {
-        // Complete the function
-        if(a.length() != b.length()) {
-            return false;
-        }
-        a = a.toLowerCase();
-        b = b.toLowerCase();
-
-        HashMap<Character, Integer> hashMap1 = new HashMap<>();
-        HashMap<Character, Integer> hashMap2 = new HashMap<>();
-
-        for(int i = 0; i < a.length(); i++) {
-            hashMap1.merge(a.charAt(i), 1, Integer::sum);
-        }
-
-        for(int i = 0; i < b.length(); i++) {
-            hashMap2.merge(b.charAt(i), 1, Integer::sum);
-        }
-
-        for(int i = 0; i < hashMap1.size(); i++) {
-            char charI = b.charAt(i);
-            if(!hashMap1.containsKey(charI) || !hashMap1.get(charI).equals(hashMap2.get(charI))) {
-                return false;
+        for(int i = 0; i < loop; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - (i+1))) {
+                flag = false;
+                break;
             }
         }
-        return true;
+
+        if (flag) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
     }
 }
