49ee38e1ed70ff9303b978be6d3074683fccbe30
Alexander Ang <alexanderang.24@gmail.com>
2020-11-11 15:51:55 +0800

    Pattern Syntax Checker

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,48 +2,25 @@ package com.example.demo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        // Write your code here.
-        ArrayList<String> list = new ArrayList<>();
-        String temp = "";
-
-        for (int i = 0; i < s.length(); i++) {
-            String charAt = String.valueOf(s.charAt(i));
-            if(!charAt.matches("[ !,?._'@]")) {
-                temp = temp + charAt;
-            } else if (!temp.equals("")) {
-                list.add(temp);
-                temp = "";
-                continue;
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            String pattern = in.nextLine();
+            //Write your code
+            try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch(Exception e) {
+                System.out.println("Invalid");
             }
-            if(i == s.length() - 1 && !temp.equals("")) {
-                list.add(temp);
-            }
+            testCases--;
         }
-
-        System.out.println(list.size());
-        for (String a: list) {
-            System.out.println(a);
-        }
-        scan.close();
-
-        // Simpler solution using String.split
-//        if (s.trim().length() == 0 || s.trim().length() > 400000) {
-//            System.out.println(0);
-//            return;
-//        }
-//        String words[]=s.trim().split("[ !,?.\\_'@]+");
-//        System.out.println(words.length);
-//        for (String word: words) {
-//            System.out.println(word);
-//        }
     }
 }
