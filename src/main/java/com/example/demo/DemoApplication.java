bb8f0e6746b7adaad491fad3cee58894fd1686e2
Alexander Ang <alexanderang.24@gmail.com>
2020-11-27 17:27:14 +0800

    Java Primality Test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,32 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+import java.math.BigInteger;
 import java.util.Scanner;
 
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String []args){
-        //Input
-        Scanner sc= new Scanner(System.in);
-        int n=sc.nextInt();
-        String []s=new String[n+2];
-        for(int i=0;i<n;i++){
-            s[i]=sc.next();
-        }
-        sc.close();
+    private static final Scanner scanner = new Scanner(System.in);
 
-        //Write your code here
-        Arrays.sort(s, 0, n, Collections.reverseOrder(Comparator.comparing(BigDecimal::new)));
-
-        //Output
-        for(int i=0;i<n;i++)
-        {
-            System.out.println(s[i]);
+    public static void main(String[] args) {
+        String n = scanner.nextLine();
+        BigInteger a = new BigInteger(n);
+        boolean prime = a.isProbablePrime(1);
+        if(prime) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
         }
+        scanner.close();
     }
 }
