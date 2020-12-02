5f97d4625b87f4ba8b765eb7dae3f0a108bae327
Alexander <alexander@doku.com>
2020-12-02 18:09:49 +0800

    Java 2D Array

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,21 +7,34 @@ import java.util.Scanner;
 @SpringBootApplication
 public class DemoApplication {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        int[][] arr = new int[6][6];
 
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = scan.nextInt();
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
         }
 
-        scan.close();
+        int max = Integer.MIN_VALUE;
+        int temp;
 
-        // Prints each sequential element in array a
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
+        for (int i = 0; i <= arr.length - 3; i++) {
+            for (int j = 0; j <= arr.length - 3; j++) {
+                temp = arr[i][j] + arr[i][j+1] +arr[i][j+2] +arr[i+1][j+1] +arr[i+2][j] +arr[i+2][j+1] +arr[i+2][j+2];
+                if (temp > max) {
+                    max = temp;
+                }
+            }
         }
+        System.out.println(max);
+
+        scanner.close();
     }
 }
