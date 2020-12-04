2499f3782da75ccac4fabfafaaf7029115e50d08
Alexander <alexander@doku.com>
2020-12-04 20:33:45 +0800

    Java Subarray

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,34 +7,51 @@ import java.util.Scanner;
 @SpringBootApplication
 public class DemoApplication {
 
-    private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-        int[][] arr = new int[6][6];
-
-        for (int i = 0; i < 6; i++) {
-            String[] arrRowItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            for (int j = 0; j < 6; j++) {
-                int arrItem = Integer.parseInt(arrRowItems[j]);
-                arr[i][j] = arrItem;
-            }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int maxLength = sc.nextInt();
+        int[] arr = new int[maxLength];
+        for (int i = 0; i < maxLength; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        int max = Integer.MIN_VALUE;
-        int temp;
+        int res = 0;
+        int temp = 0;
+        int length = 1;
+        int times = 0;
 
-        for (int i = 0; i <= arr.length - 3; i++) {
-            for (int j = 0; j <= arr.length - 3; j++) {
-                temp = arr[i][j] + arr[i][j+1] +arr[i][j+2] +arr[i+1][j+1] +arr[i+2][j] +arr[i+2][j+1] +arr[i+2][j+2];
-                if (temp > max) {
-                    max = temp;
+        while (length <= maxLength) {
+            for (int i = 0; i + length <= maxLength; i++) {
+                for (int j = i; times < length; j++, times++) {
+                    temp += arr[j];
+                }
+                if (temp < 0) {
+                    res++;
+                }
+                times = 0;
+                temp = 0;
+            }
+            length++;
+        }
+
+        System.out.println(res);
+        sc.close();
+    }
+
+    // Simpler solution
+    private static int negativeSubarrays(int [] array) {
+        int count = 0;
+        int sum   = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j < array.length; j++) {
+                sum += array[j];
+                if (sum < 0) {
+                    count++;
                 }
             }
+            sum = 0;
         }
-        System.out.println(max);
-
-        scanner.close();
+        return count;
     }
 }
