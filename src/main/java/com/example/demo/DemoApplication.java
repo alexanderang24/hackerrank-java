174eb83cb27a7c712aa1e3d4ccf7ad8aef6c6e8d
Alexander <alexander@doku.com>
2020-12-10 12:41:36 +0800

    Java Arraylist

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -9,49 +10,31 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner sc = new Scanner(System.in);
-        int maxLength = sc.nextInt();
-        int[] arr = new int[maxLength];
-        for (int i = 0; i < maxLength; i++) {
-            arr[i] = sc.nextInt();
-        }
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
-        int res = 0;
-        int temp = 0;
-        int length = 1;
-        int times = 0;
+        ArrayList<int[]> arrayList = new ArrayList<>();
 
-        while (length <= maxLength) {
-            for (int i = 0; i + length <= maxLength; i++) {
-                for (int j = i; times < length; j++, times++) {
-                    temp += arr[j];
-                }
-                if (temp < 0) {
-                    res++;
-                }
-                times = 0;
-                temp = 0;
+        for (int i = 0; i < n; i++) {
+            int[] temp  = new int[scanner.nextInt()];
+            for (int j = 0; j < temp.length; j++) {
+                temp[j] = scanner.nextInt();
             }
-            length++;
+            arrayList.add(temp);
         }
 
-        System.out.println(res);
-        sc.close();
+        int totalQueries = scanner.nextInt();
+        for (int i = 0; i < totalQueries; i++) {
+            find(scanner.nextInt()-1, scanner.nextInt()-1, arrayList);
+        }
     }
 
-    // Simpler solution
-    private static int negativeSubarrays(int [] array) {
-        int count = 0;
-        int sum   = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i; j < array.length; j++) {
-                sum += array[j];
-                if (sum < 0) {
-                    count++;
-                }
-            }
-            sum = 0;
+    private static void find(int x, int y, ArrayList<int[]> list) {
+        int[] row = list.get(x);
+        if(y >= row.length) {
+            System.out.println("ERROR!");
+        } else {
+            System.out.println(row[y]);
         }
-        return count;
     }
 }
